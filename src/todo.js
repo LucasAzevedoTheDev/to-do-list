7c5c2be3ec7d21141881ea8781a851ac54e67365
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-08-13 20:53:03 -0300

    create project class

--- src/todo.js
+++ src/todo.js
@@ -1,13 +1,22 @@
 class Todo {
   checklist = false;
-  
-  constructor(title, description, dueDate, priority, notes, checklist) {
+
+  constructor(title, description, dueDate, priority, notes) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.notes = notes;
-    this.checklist = checklist;
   }
 }
 
+class Project {
+  
+  constructor(name) {
+    this.name = name;
+    this.todos = [];
+  }
+}
+// i need to have projects ✅
+// Users should be able to create new projects ✅
+//  and choose which project their todos go into 
