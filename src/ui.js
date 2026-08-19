c52fc0d8091a85af72b3a13110cf45e36d0e11e1
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-08-19 16:35:35 -0300

    make createTodoDiv function

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,5 @@
 import "./styles.css";
 
-
 const containerDiv = document.createElement("div");
 containerDiv.classList.add("container-div");
 
@@ -9,10 +8,37 @@ function createContainer() {
   body.appendChild(containerDiv);
 }
 
-function createTodo() {
-  // 
+function createTodoDiv(todo) {
+  const todoDiv = document.createElement("div");
+  containerDiv.appendChild(todoDiv);
+
+  const todoTitle = document.createElement("p");
+  todoTitle.classList.add("todo-title");
+  todoTitle.textContent = todo.title;
+
+  const todoDescription = document.createElement("p");
+  todoDescription.classList.add("todo-description");
+  todoDescription.textContent = todo.description;
+
+  const todoDate = document.createElement("p");
+  todoDate.classList.add("todo-date");
+  todoDate.textContent = todo.dueDate;
+
+  const todoPriority = document.createElement("p");
+  todoPriority.classList.add("todo-priority");
+  todoPriority.textContent = todo.priority;
+
+  const todoNotes = document.createElement("p");
+  todoNotes.classList.add("todo-notes");
+  todoNotes.textContent = todo.notes;
+
+  todoDiv.appendChild(todoTitle);
+  todoDiv.appendChild(todoDescription);
+  todoDiv.appendChild(todoDate);
+  todoDiv.appendChild(todoPriority);
+  todoDiv.appendChild(todoNotes);
 }
 
-export {createContainer, createTodo};
+export {createContainer, createTodoDiv};
 
 
