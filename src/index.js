fd77cffbefdf14113d80c752166e2070486dd744
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-08-19 17:06:07 -0300

    set up css reset rules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 import {addProject, addTodoToProject, projects} from "./todo.js";
 import {toggleCompleted, deleteTodo, deleteProject} from "./edit.js";
-import {createContainer, createTodo} from "./ui.js";
+import {createContainer, createTodoDiv} from "./ui.js";
 
 const defaultProject = addProject("default");
 const todo1 = addTodoToProject("walk", "walk the dog", "08/15", "medium", "no additional notes", defaultProject);
@@ -9,12 +9,6 @@ const todo1 = addTodoToProject("walk", "walk the dog", "08/15", "medium", "no ad
 const mainProject = addProject("mainProject");
 const todo2 = addTodoToProject("drink", "water", "tomorrow", "max", "no additional notes", defaultProject);
 
-console.log(defaultProject);
-// console.log(projects);
-// console.log(mainProject);
-
-deleteTodo(todo1);
-
-console.log(defaultProject);
+createContainer();
 
 
