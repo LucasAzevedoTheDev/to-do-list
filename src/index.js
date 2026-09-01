4a72f20d1a477b85bcc35ec246c146fab880bf65
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-08-31 21:01:54 -0300

    add comments

--- src/index.js
+++ src/index.js
@@ -13,8 +13,9 @@ createTodoDiv(todo2);
 const todo3 = addTodoToProject("test 3", "bliblibli", formatDate("2026-12-11T16:35"), "high", defaultProject);
 createTodoDiv(todo3);
 
+// enable the todoDialog to edit the todooo
+// add localStorage feature
 
-// add delete modal button 
 
 
 
