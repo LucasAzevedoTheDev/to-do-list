fa634a5c30d0927d56945583e1f95eadc82fbad4
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-09-10 19:03:05 -0300

    remove unnecessary lines

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import {addProject, addTodoToProject, projects, priority, addDataToLocalStorage,
 import {createContainer, createTodoDiv} from "./ui.js";
 import {formatDate} from "./edit.js";
 
-console.log(projects.length);
 if(projects.length === 0) {
   addProject("General");
 }
-console.log(projects.length); 
-createContainer();
 
-let current = localStorage;
-// console.log(current);
+createContainer();
 
 window.addEventListener('DOMContentLoaded', () => {
     const navigationEntries = performance.getEntriesByType('navigation');
@@ -20,13 +16,9 @@ window.addEventListener('DOMContentLoaded', () => {
         const navigationType = navigationEntries[0].type;
         
         if (navigationType === 'reload') {
-            console.log('This page was refreshed!');
             addDataToLocalStorage();
             addLocalStoredProjects();
-            console.log(projects.length);
-        } else {
-            console.log('This is a fresh page load (or navigated via link/history).');
-        }
+        } 
     }
 });
 
